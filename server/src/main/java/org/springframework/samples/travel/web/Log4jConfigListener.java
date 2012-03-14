793b20427e13723509fedf92966e2b98291c358a
Peter Ledbrook <peter@cacoethes.co.uk>
2012-03-14 12:52:48 +0000

    Set up AMPQ appender with log file and override for Cloud Foundry.
    Rather than programmatically building the AMQP appender for both local and cloud environments, the application now configures it in log4j.xml and then overrides just the connection factory settings for Cloud Foundry.

--- server/src/main/java/org/springframework/samples/travel/web/Log4jConfigListener.java
+++ server/src/main/java/org/springframework/samples/travel/web/Log4jConfigListener.java
@@ -3,7 +3,6 @@ package org.springframework.samples.travel.web;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.cloudfoundry.runtime.env.CloudEnvironment;
 import org.cloudfoundry.runtime.env.RabbitServiceInfo;
@@ -17,50 +16,33 @@ public class Log4jConfigListener implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent event) {
 		Log4jWebConfigurer.initLogging(event.getServletContext());
 
-		AmqpAppender amqpAppender;
 		CloudEnvironment cloudEnvironment = new CloudEnvironment();
 		if (cloudEnvironment.isCloudFoundry()) {
-			amqpAppender = createCloudFoundryAppender(cloudEnvironment);
+			initCloudFoundryAppender(cloudEnvironment);
 		}
-		else {
-			amqpAppender = createLocalAppender();
-		}
-		
-		Logger.getRootLogger().addAppender(amqpAppender);
 	}
  
 	public void contextDestroyed(ServletContextEvent event) {
 		Log4jWebConfigurer.shutdownLogging(event.getServletContext());
 	}
 	
-	AmqpAppender createCloudFoundryAppender(CloudEnvironment env) {
-		RabbitServiceInfo rabbitServiceInfo = env.getServiceInfo("rabbitmq-logs", RabbitServiceInfo.class);
-		AmqpAppender amqpAppender = createAmqpAppender();
-		amqpAppender.setHost(rabbitServiceInfo.getHost());
-		amqpAppender.setPort(rabbitServiceInfo.getPort());
-		amqpAppender.setVirtualHost(rabbitServiceInfo.getVirtualHost());
-		amqpAppender.setUsername(rabbitServiceInfo.getUserName());
-		amqpAppender.setPassword(rabbitServiceInfo.getPassword());
-		return amqpAppender;
+	void initCloudFoundryAppender(CloudEnvironment env) {
+		try {
+			RabbitServiceInfo rabbitServiceInfo = env.getServiceInfo("rabbitmq-logs", RabbitServiceInfo.class);
+			AmqpAppender amqpAppender = getAmqpAppender();
+			amqpAppender.close();                                 // Required for changes to take effect
+			amqpAppender.setHost(rabbitServiceInfo.getHost());
+			amqpAppender.setPort(rabbitServiceInfo.getPort());
+			amqpAppender.setVirtualHost(rabbitServiceInfo.getVirtualHost());
+			amqpAppender.setUsername(rabbitServiceInfo.getUserName());
+			amqpAppender.setPassword(rabbitServiceInfo.getPassword());
+		}
+		catch (Throwable t) {
+			t.printStackTrace();
+		}
 	}
 	
-	AmqpAppender createLocalAppender() {
-		AmqpAppender amqpAppender = createAmqpAppender();
-		amqpAppender.setHost("localhost");
-		amqpAppender.setVirtualHost("/");
-		amqpAppender.setUsername("guest");
-		amqpAppender.setPassword("guest");
-		return amqpAppender;
-	}
-	
-	AmqpAppender createAmqpAppender() {
-		AmqpAppender amqpAppender = new AmqpAppender();
-		amqpAppender.setName("rabbit");
-		amqpAppender.setExchangeName("amq.topic");
-		amqpAppender.setExchangeType("topic");
-		amqpAppender.setRoutingKeyPattern("logs.spring-travel");
-		amqpAppender.setApplicationId("spring-travel");
-		amqpAppender.setThreshold(Level.INFO);
-		return amqpAppender;
+	AmqpAppender getAmqpAppender() {
+		return (AmqpAppender) Logger.getRootLogger().getAppender("amqp");
 	}
 }
